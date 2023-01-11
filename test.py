7dbd8e7adc9ab80957d2c75bd34e83ba47a4912d
hkison <hyunkison98@gmail.com>
2023-01-11 11:42:35 +0900

    commit

---
+++ test.py
@@ -0,0 +1,34 @@
+## print("hello world")
+
+# ctrl + F5 (실행)
+#외부 터미널 실행:  1. 인터프리터 세팅 확인
+#                   2. [실행] - [구성추가]
+#                   3. integratedTerminal 만 externalTerminal 로 바꾸기
+
+#print(1)
+#print(0.5)
+#print(-0.33)
+#print(2-3j)
+
+#print(0o20)
+#print(0o111)
+# python 은 0으로 시작할수없다. (0123) 이런거 안됨. 진법표기는 제외(0b12, 0x1F, ...)
+
+#print('s')
+#print("hello")
+#print('world')
+#print('1')
+#print("2")
+#print("1+1")
+#print(1+1)
+#print('mother\'s birthday')
+#print("mother's birthday")
+#print('mother's birthday') <-출력안됨
+
+#print("이름\t손현기")
+#print("나이\t26")
+#print("이메일\thyunkison98@gmail.com")
+#print("동해물과\n백두산이\n마르고\n닳도록")
+#print('mother\'s birthday')
+#print("\"")
+#print("\\")
---
+++ test2.py
@@ -0,0 +1,97 @@
+#print("제 나이는",26,"\b입니다 ")
+
+#print(1,end = ",") <-print()는 명령 이후에 줄바꿈이 자동으로 되어있음
+#print(2)
+#print(3)
+
+#print("제 나이는",37,"입니다",sep="\n") <- 자료형 간 공백도 자동
+
+# print(1)  <- 주석 단축키 : ctrl + /
+
+'''동해물과
+백두산이
+마르고
+닳도록
+'''
+
+"""1
+2
+3
+4
+"""
+
+# 여러줄 문자열 <- 주석이 아님! 프로그램에 영향을 미침
+
+# print("""
+# 왜들 그리 다운돼있어? 뭐가 문제야 say something
+# 분위기가 겁나 싸해 요새는 이런 게 유행인가
+# 왜들 그리 재미없어? 아 그건 나도 마찬가지
+# Tell me what I got to do 급한 대로 블루투스 켜
+# 아무 노래나 일단 틀어 아무거나 신나는 걸로
+# 아무렇게나 춤춰 아무렇지 않아 보이게
+# 아무 생각 하기 싫어 아무개로 살래 잠시
+# I'm sick and tired of my every day, keep it up 한 곡 더
+# """)
+
+# x = 10
+
+# print(x)
+# print("x의 값은",x,"입니다")
+
+# y = "abc"
+# print(y)
+# print("y")
+
+#1a = 10 <- 숫자시작 안됨
+# total money = 10 <-공백 안됨
+
+#print는 함수로 사용할 수 있지만 한번 사용하면 더이상 print 기능을 사용할 수 없다
+
+name = "손현기"
+age = 25
+email = "hyunkison98@gmail.com"
+
+# print("제 이름은",name,"이고 \n나이는",age,"살 입니다.\n저의 이메일은",email,"입니다.")
+
+# type(1)
+# type(1.2)
+# type(-3)
+# type(-2j)
+# type('a')
+# type('hello')
+
+# print(type(1))
+# print(type("hello"))
+
+# a = 10
+# b = 3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)  #나머지
+# print(a//b) #몫
+# print(a**b) #제곱
+
+
+# A="hello" # A=20
+# B="world" # B=3
+
+# print("A 는",A,"입니다.")
+# print("B 는",B,"입니다.\n") # \n 대신 print()도 사용가능
+# print("A + B =",A+B)
+# print("A - B =",A-B)
+# print("A * B =",A*B)      # 문자열 반복은 가능하다 A * 3
+# print("A / B =",A/B)
+# print("A // B =",A//B)
+# print("A % B =",A%B)
+# print("A ** B =",A**B)
+
+#2로 나눈 나머지가 0이거나 1일때 -> 짝수나 홀수를 구분하기 위해 사용할 수 있음
+#나머지가 0일때 -> 약수를 구할 수 있음 : A가 B의 약수이다! -> A%B=0
+#A가 B의 배수이다! -> B%A=0
+
+#반복 -> %를 이용
+
+

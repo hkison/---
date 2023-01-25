b9baf18662bfbee6ee26a0bc4c22e3092aefbc0d
hkison <hyunkison98@gmail.com>
2023-01-25 14:18:59 +0900

    Create my own game day3

--- MyOwnGame.py
+++ MyOwnGame.py
@@ -7,6 +7,7 @@
 # 1억 달성시 승리
 # 잔금이 0원이거나 그 이하일시 대출 1회 가능(라이프 개념)
 # 그 이후 다시한번 총 자산이 0원 이하면 패배
+# 30초 안에 매수나 매도 하지않으면 자동으로 보류 -> 다음 라운드 이동(코인 등락)
 import time
 import os
 import random
@@ -21,20 +22,129 @@ Cp = 4000
 CpPer = 0
 Cs = 1000
 CsPer = 0
-SeedMoney = 1_000_000
+
+seedMoney = 1000000
+seedC = 0
+seedPy = 0
+seedJ = 0
+seedCp = 0
+seedCs = 0
+
+buy = 1
+sell = 2
+
+upDownPer = 50
+
+upDownCntC = 0
+upDownCntPy = 0
+upDownCntJ = 0
+upDownCntCp = 0
+upDownCntCs = 0
 
 while True:
     # time.sleep(0.1)
     os.system("cls")
-    print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓")
-    print("┃ 오늘의 비트코인 시세","%14s" % " ","┃")
-    print("┃","%35s" % " ","┃")
-    print("┃ C코인","%20s %7s" % (C,CPer),"%┃")
-    print("┃ 파이썬코인","%15s %7s" % (Py,PyPer),"%┃")
-    print("┃ 자바코인","%17s %7s" % (J,JPer),"%┃")
-    print("┃ C++코인","%18s %7s" % (Cp,CpPer),"%┃")
-    print("┃ C#코인","%19s %7s" % (Cs,CsPer),"%┃")
-    print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
+    print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓")
+    print("┃ 오늘의 비트코인 시세","%26s" % " ","┃보유 코인 현황  ┃")
+    print("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━┫")
+    print("┃ 코인종류          등락확률(%) 현재시세 등락률(%)┃""                ┃")
+    print("┃","%47s" % " ","┃                ┃")
+    print("┃ 1.C코인","%19s %10s %7s" % (upDownPer,C,CPer),"%┃","%14s" % (seedC), "┃")
+    print("┃ 2.파이썬코인","%14s %10s %7s" % (upDownPer,Py,PyPer),"%┃","%14s" % (seedPy),"┃")
+    print("┃ 3.자바코인","%16s %10s %7s" % (upDownPer,J,JPer),"%┃","%14s" % (seedJ),"┃")
+    print("┃ 4.C++코인","%17s %10s %7s" % (upDownPer,Cp,CpPer),"%┃","%14s" % (seedCp),"┃")
+    print("┃ 5.C#코인","%18s %10s %7s" % (upDownPer,Cs,CsPer),"%┃","%14s" % (seedCs),"┃")
+    print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┛")
+
+    print("매수 : 1번\t\t 현재금액 : ",end ="")
+    print("{:,}".format(seedMoney))
+    print("매도 : 2번")
+
+    def coinBuyLogic(a,b):
+        global coinAmount
+        global seedMoney
+        global seedC
+        print("현재 최대 가능 매수 : ", seedMoney // b)
+        coinAmount = input("얼마나 매수하시겠습니까? : ")
+        try :
+            if int(coinAmount) > seedMoney // b :
+                print("보유하신 금액이 부족합니다")
+                time.sleep(2)
+            elif (coinAmount).isnumeric():
+                print(coinAmount,"개 매수 완료.")
+                a += int(coinAmount)
+                seedMoney -= int(coinAmount) * b
+                print("{:,}".format(int(coinAmount) * b * -1))
+                print("현재 보유 금액 : ""{:,}".format(seedMoney))
+                time.sleep(2)
+            else:
+                print("숫자를 입력해주세요.")
+                time.sleep(1)
+        except:
+            print("에러 ! 유효하지 않은 메뉴나 값입니다")
+            time.sleep(1.5)
+
+    def coinSellLogic(a,b):
+        global coinAmount
+        global seedMoney
+        print("현재 최대 가능 매도 : ", a)
+        coinAmount = input("얼마나 매도하시겠습니까? : ")
+        try :
+            if a < int(coinAmount) :
+                print("보유하신 코인이 부족합니다")
+                time.sleep(2)
+            elif (coinAmount).isnumeric():
+                print(coinAmount,"개 매도 완료.")
+                a -= int(coinAmount)
+                seedMoney += int(coinAmount) * b
+                print("{:,}".format(int(coinAmount) * b ))
+                print("현재 보유 금액 : ""{:,}".format(seedMoney))
+                time.sleep(2)
+            else:
+                print("숫자를 입력해주세요.")
+                time.sleep(1)
+        except:
+            print("에러 ! 유효하지 않은 메뉴나 값입니다")
+            time.sleep(1.5)
+
+    user = input("메뉴를 선택하세요 : ")
+    if user == '1':
+        coinKind = input("매수 종목 선택 : ")
+        if coinKind == '1':
+            coinBuyLogic(seedC,C)
+        elif coinKind == '2':
+            coinBuyLogic(seedPy,Py)
+        elif coinKind == '3':
+            coinBuyLogic(seedJ,J)
+        elif coinKind == '4':
+            coinBuyLogic(seedCp,Cp)
+        elif coinKind == '5':
+            coinBuyLogic(seedCs,Cs)
+        else:
+            print("번호를 입력하여 주십시오.")
+            time.sleep(2)
+    elif user == '2':
+        if (seedCp and seedC and seedCs and seedJ and seedPy) == 0:
+            print("보유하신 코인이 없습니다")
+            time.sleep(1.4)
+        else:
+            coinMenu = int(input("매도 종목 선택 : "))
+            if coinKind == '1':
+                coinSellLogic(seedC,C)
+            elif coinKind == '2':
+                coinSellLogic(seedPy,Py)
+            elif coinKind == '3':
+                coinSellLogic(seedJ,J)
+            elif coinKind == '4':
+                coinSellLogic(seedCp,Cp)
+            elif coinKind == '5':
+                coinSellLogic(seedCs,Cs)
+            else:
+                print("번호를 입력하여 주십시오.")
+                time.sleep(2)
+    else:
+        print("번호를 맞게 입력하여 주십시오.")
+        time.sleep(2)
 
     CPer =  random.randint(-90,90)
     PyPer =  random.randint(-50,50)
@@ -48,6 +158,5 @@ while True:
     Cp += int(Cp*CPer/100)
     Cs += int(Cs*CsPer/100)
 
-    time.sleep(5)
-    os.system("cls")
-
+    
+    
